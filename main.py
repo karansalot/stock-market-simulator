6ea2de55d004051b2c6bee5d8480b4107d5f412a
Karan Salot <101121492+karansalot@users.noreply.github.com>
2025-02-14 03:16:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -158,3 +158,12 @@ def leaderboard():
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
+
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
