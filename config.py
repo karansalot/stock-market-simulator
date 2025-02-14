65611afab8cbe91b2ebd0337951fb54630dfdd0d
Karan Salot <101121492+karansalot@users.noreply.github.com>
2025-02-14 03:20:12 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'your_database_uri'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # Fetch from environment variables
     SQLALCHEMY_TRACK_MODIFICATIONS = False
